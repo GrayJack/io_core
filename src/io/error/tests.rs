e0cbefd23d975ddaacf301996c91141856b06300
GrayJack <gr41.j4ck@gmail.com>
2026-08-29 02:28:19 -0300

    tests: Fix tests on no_std

--- src/io/error/tests.rs
+++ src/io/error/tests.rs
@@ -3,7 +3,9 @@ use crate::io_const_error;
 
 use core::{assert_matches, error, fmt};
 
-use crate::sys::io::{decode_error_kind, error_string};
+use crate::sys::io::decode_error_kind;
+#[cfg(feature = "std")]
+use crate::sys::io::error_string;
 
 #[cfg(feature = "alloc")]
 use alloc::{boxed::Box, string::ToString};
